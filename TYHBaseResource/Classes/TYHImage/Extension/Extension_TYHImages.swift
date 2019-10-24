8bddd9875310c2dfcb0c78156d5e35ab69691177
YaoHua Tan <yaohuatan@Shinkai.local>
2019-10-24 14:31:14 +0800

    first

---
+++ TYHBaseResource/Classes/TYHImage/Extension/Extension_TYHImages.swift
@@ -0,0 +1,30 @@
+//
+//  Extension_TYHImages.swift
+//  TYHResource
+//
+//  Created by YaoHua Tan on 2019/10/24.
+//  Copyright © 2019 YaoHua Tan. All rights reserved.
+//
+
+import UIKit
+
+class Extension_TYHImages: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TYHResource/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TYHResource
+//
+//  Created by YaoHua Tan on 2019/10/24.
+//  Copyright © 2019 YaoHua Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
