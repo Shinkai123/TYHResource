63352bcb4888017909c850967182770e62f2f4e8
YaoHua Tan <yaohuatan@Shinkai.local>
2019-10-24 17:30:12 +0800

    add resource

--- TYHBaseResource/Classes/TYHImage/Extension/Extension_TYHImages.swift
+++ TYHBaseResource/Classes/TYHImage/Extension/Extension_TYHImages.swift
@@ -6,25 +6,32 @@
 //  Copyright © 2019 YaoHua Tan. All rights reserved.
 //
 
-import UIKit
+import CTMediator
 
-class Extension_TYHImages: UIViewController {
+//模块名称
+let TYHImageModuleName = "TYHBaseResource"
+//标识
+let TYHImageName = "TYHImages"
+//获取图片
+let TYHResourceGetImage = "resourceGetImage"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+public extension CTMediator {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tyh_resourceGetImage(imageName: String, modular: String) -> UIImage? {
+        let params = [kCTMediatorParamsKeySwiftTargetModuleName:TYHImageModuleName,
+                      "imageName": imageName,
+                      "modular": modular] as [AnyHashable: Any];
+        return performTarget(TYHImageName, action: TYHResourceGetImage, params: params, shouldCacheTarget: false) as? UIImage
     }
-    */
+}
 
+public let TYHResourceAssetsName_Home = "Home"
+
+public enum TYHResourceAssetsName: String {
+    //MARK: -Home
+    case icon_preview_add_no
+    case icon_preview_add_pre
+    case icon_preview_menu_no
+    case icon_preview_menu_pre
 }
